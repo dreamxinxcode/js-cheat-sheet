d024bd3991e0aa29992f73ca25e8c6bbb93fac27
dreamxinxcode <dreamxinxcode@gmail.com>
2020-09-01 12:57:14 -0700

    added array contructor

--- arrays.js
+++ arrays.js
@@ -1,5 +1,8 @@
-let arr = [1, 2, 3, 4, 5];
+// Array constructor
+let array = new Array(1, 2, 3, 4, 5);
 
+// Array literal
+let arr = [1, 2, 3, 4, 5];
 
 // Create new copy (spread operator)
 let newArr = [... arr];
